b74b1c1f38edee4fb407472790c9e652fcab9757
Daniel Real <REALDI1@student.op.ac.nz>
2020-04-20 14:29:40 +1200

    Made Image table

--- Js/Script.js
+++ Js/Script.js
@@ -7,16 +7,5 @@ function classToggle() {
   document.querySelector('.Navbar__Link-toggle')
     .addEventListener('click', classToggle);
 
-function recentSonglist() {
-  fetch("../json/Music.json")
-.then(response => { return response.json() })
-.then(data => {
-    let music = data[""];
-    var Recentsongs = document.getElementsByClassName('RSongs')
-  for (let i=0; i < data[Recent_Songs].Length; i++)
-  {
-    Recentsongs[i].innerHTML = ("<img src= (data[Recent_Songs][Images][i])><p>(data[Recent_Songs][Name][i])</p><p>(data[Recent_Songs][Artist][i])</p><p>(data[Recent_Songs][Last_Listened][i])</p>")
-  }
-})
-}
+
 
