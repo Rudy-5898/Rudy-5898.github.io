5bc369f79f3b57205c18d28ca61f6cbd618c391e
Daniel Real <REALDI1@student.op.ac.nz>
2020-04-24 17:45:15 +1200

    Added heart functionality

--- Js/Script.js
+++ Js/Script.js
@@ -7,5 +7,132 @@ function classToggle() {
   document.querySelector('.Navbar__Link-toggle')
     .addEventListener('click', classToggle);
 
+function heart1Function() {
+ let heart=document.getElementById("heart1")
+console.log(heart)
+if (heart.style.filter == "brightness(100%)")
+{
+heart.style.filter = "brightness(50%)";
+}
+else 
+{
+  heart.style.filter = "brightness(100%)";
+}
+}
 
+function heart2Function() {
+  let heart=document.getElementById("heart2")
+ console.log(heart)
+ if (heart.style.filter == "brightness(100%)")
+ {
+ heart.style.filter = "brightness(50%)";
+ }
+ else 
+ {
+   heart.style.filter = "brightness(100%)";
+ }
+ }
 
+ function heart3Function() {
+  let heart=document.getElementById("heart3")
+ console.log(heart)
+ if (heart.style.filter == "brightness(100%)")
+ {
+ heart.style.filter = "brightness(50%)";
+ }
+ else 
+ {
+   heart.style.filter = "brightness(100%)";
+ }
+ }
+
+ function heart4Function() {
+  let heart=document.getElementById("heart4")
+ console.log(heart)
+ if (heart.style.filter == "brightness(100%)")
+ {
+ heart.style.filter = "brightness(50%)";
+ }
+ else 
+ {
+   heart.style.filter = "brightness(100%)";
+ }
+ }
+
+ function heart5Function() {
+  let heart=document.getElementById("heart5")
+ console.log(heart)
+ if (heart.style.filter == "brightness(100%)")
+ {
+ heart.style.filter = "brightness(50%)";
+ }
+ else 
+ {
+   heart.style.filter = "brightness(100%)";
+ }
+ }
+
+ function heart6Function() {
+  let heart=document.getElementById("heart6")
+ console.log(heart)
+ if (heart.style.filter == "brightness(100%)")
+ {
+ heart.style.filter = "brightness(50%)";
+ }
+ else 
+ {
+   heart.style.filter = "brightness(100%)";
+ }
+ }
+
+ function heart7Function() {
+  let heart=document.getElementById("heart7")
+ console.log(heart)
+ if (heart.style.filter == "brightness(100%)")
+ {
+ heart.style.filter = "brightness(50%)";
+ }
+ else 
+ {
+   heart.style.filter = "brightness(100%)";
+ }
+ }
+
+ function heart8Function() {
+  let heart=document.getElementById("heart8")
+ console.log(heart)
+ if (heart.style.filter == "brightness(100%)")
+ {
+ heart.style.filter = "brightness(50%)";
+ }
+ else 
+ {
+   heart.style.filter = "brightness(100%)";
+ }
+ }
+
+ function heart9Function() {
+  let heart=document.getElementById("heart9")
+ console.log(heart)
+ if (heart.style.filter == "brightness(100%)")
+ {
+ heart.style.filter = "brightness(50%)";
+ }
+ else 
+ {
+   heart.style.filter = "brightness(100%)";
+ }
+ }
+
+ function heart10Function() {
+  let heart=document.getElementById("heart10")
+ console.log(heart)
+ if (heart.style.filter == "brightness(100%)")
+ {
+ heart.style.filter = "brightness(50%)";
+ }
+ else 
+ {
+   heart.style.filter = "brightness(100%)";
+ }
+ }
