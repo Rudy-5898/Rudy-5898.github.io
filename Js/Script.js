8485e109bba6837e2050f620ce45d6900b474f2c
Daniel Real <REALDI1@student.op.ac.nz>
2020-04-25 12:27:20 +1200

    Removed JQuery

--- Js/Script.js
+++ Js/Script.js
@@ -1,3 +1,42 @@
+fetch ('data.json')
+        .then(response => {return response.json() })
+        .then(data => {
+          let music = data['Music']
+         
+          let griddivs = (document.getElementsByClassName('imgcontainer'));
+          let gridTextdivs = (document.getElementsByClassName("grid-text"));
+          for (let i =0; i < griddivs.length; i++)
+          {
+            griddivs[i].style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("+(data['Music'][0]['Artists'][i]['Images'])+")";
+            griddivs[i].style.backgroundColor = "#2f2f2f";
+            gridTextdivs[i].innerHTML= (data['Music'][0]["Artists"][i]['Name'])+", "+(data['Music'][0]["Artists"][i]['Listens'])+" Total Listens";
+          }
+          
+          let songdivs =(document.getElementsByClassName('Topsong-name'));
+          let artistdivs=(document.getElementsByClassName('Topsong-artist'))
+          let listensdivs=(document.getElementsByClassName('Topsong-listens'))
+          let bardivs=(document.getElementsByClassName('barchart'))
+          console.log(songdivs);
+          for (let i=0; i < songdivs.length; i++)
+          {
+            songdivs[i].innerHTML= (data['Music'][1]['Top_Songs'][i]['Name'])
+            artistdivs[i].innerHTML= (data['Music'][1]['Top_Songs'][i]['Artist'])
+            listensdivs[i].innerHTML= (data['Music'][1]['Top_Songs'][i]['Listens'])
+            bardivs[i].style.width= (((data['Music'][1]['Top_Songs'][i]['Listens'])/100)*30)+"%";
+          }
+          let rsongdivs=(document.getElementsByClassName("Recentsong-name"))
+          let rartistdivs=(document.getElementsByClassName("Recentsong-artist"))
+          let rimgdivs=(document.querySelectorAll("#recentimg"))
+          let rlistendivs =(document.getElementsByClassName("Recent-time"))
+          for (let i=0; i<rsongdivs.length; i++)
+          {
+            rsongdivs[i].innerHTML= (data['Music'][2]['Recent_Songs'][i]['Name'])
+            rartistdivs[i].innerHTML= (data['Music'][2]['Recent_Songs'][i]['Artist'])
+            rimgdivs[i].src = (data['Music'][2]['Recent_Songs'][i]['Images'])
+            rlistendivs[i].innerHTML = (data['Music'][2]['Recent_Songs'][i]['Last_Listened'])
+          }
+        })
+
 function classToggle() {
     const navs = document.querySelectorAll('.Navbar__Items')
     
